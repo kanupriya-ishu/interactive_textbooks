3798d23e305a87dc4d9b0ddc09fbfb87268bc245
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-13 02:02:35 +0530

    New Post component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
-      <Single />
+      <Write />
     </div>
   );
 }
