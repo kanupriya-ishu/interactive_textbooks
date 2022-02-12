edbfc2236ce4d13999388e675cc6305e6364b5b8
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-12 23:19:08 +0530

    Created header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/home/Home";
 import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
+      <Home />
     </div>
   );
 }
---
+++ front-end/src/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <img className="headerImg" src="https://csc4girlblog.files.wordpress.com/2017/11/laptop-book-animation.gif" alt=""></img>
+        <div className="headerTitles">
+            <span className="headerTitleLg">iKitab</span>
+            <span className="headerTitleSm">Where every reading experience turns into an interactive journey!</span>
+        </div>
+        
+    </div>
+  )
+}
