3ebfda999afe0a85519ec1b123a666a19df792b9
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-13 17:20:13 +0530

    User CRUD

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -18,6 +21,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () =>{
     console.log("Backend is running");
