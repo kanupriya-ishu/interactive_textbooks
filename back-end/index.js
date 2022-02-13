d167bf03a1f23edd0f69a3348625262ee5913890
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-13 16:19:52 +0530

    Login and Register Routes

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -15,9 +17,7 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.use("/", (req, res) => {
-    console.log("Hey this is main url");
-});
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () =>{
     console.log("Backend is running");
