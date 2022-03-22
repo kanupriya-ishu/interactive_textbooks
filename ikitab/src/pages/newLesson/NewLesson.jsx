09a249408601416df98b7c2667b8ee5b55f0a5d3
kanupriya-ishu <kanupriya2512@gmail.com>
2022-03-22 10:41:51 +0530

    Embedded editor

--- ikitab/src/pages/newLesson/NewLesson.jsx
+++ ikitab/src/pages/newLesson/NewLesson.jsx
@@ -3,11 +3,8 @@ import React, { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
-import { EditorState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import { convertToHTML } from 'draft-convert';
-import DOMPurify from 'dompurify';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import "./newLesson.css"
 
 export default function NewLesson() {
@@ -19,7 +16,76 @@ export default function NewLesson() {
     const [lesson_number, setLesson_number] = useState("");
     const [lesson_title, setLesson_title] = useState("");
     const [lesson_body, setLesson_Body] = useState("");
+    const [text,setText] = useState('');
 
+    const MyComponent = () => {
+      const [text,setText] = useState('');
+
+      const handleChange= (html)=> {
+        setText(html);
+        console.log(html);
+        setLesson_Body(html);
+      }
+
+        const imageHandler = () =>{
+        const tooltip = this.quill.theme.tooltip;
+        const originalSave = tooltip.save;
+        const originalHide = tooltip.hide;
+
+        tooltip.save = function () {
+          const range = this.quill.getSelection(true);
+          const value = this.textbox.value;
+          if (value) {
+            this.quill.insertEmbed(range.index, 'image', value, 'user');
+          }
+        };
+        // Called on hide and save.
+        tooltip.hide = function () {
+          tooltip.save = originalSave;
+          tooltip.hide = originalHide;
+          tooltip.hide();
+        };
+        tooltip.edit('image');
+        tooltip.textbox.placeholder = 'Embed URL';
+      }
+      const modules = {
+        toolbar: {
+            container: [
+                [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+                [{size: []}],
+                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                [{'list': 'ordered'}, {'list': 'bullet'}, 
+                {'indent': '-1'}, {'indent': '+1'}],
+                ['link', 'image', 'video'],
+                [{ 'color': [] }, { 'background': [] }], 
+                ['clean'],
+                [{ 'align': [] }]
+            ],
+            handlers: {
+                image: imageHandler
+            }
+        }
+      };
+      const formats = [
+        'header', 'font', 'size',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'align',
+        'list', 'bullet', 'indent',
+        'link', 'image', 'video', 'color'
+      ]
+
+      return(
+        <ReactQuill
+                  value={text}
+                  onChange={handleChange}
+                  modules={modules}
+                  formats={formats}
+                />
+      );
+    }
+    
+
+  
     const handleSubmit = async (e) => {
         e.preventDefault();
         const newLesson = {
@@ -33,41 +99,7 @@ export default function NewLesson() {
         } catch(err) {}
     };
 
-    const [editorState, setEditorState] = useState(
-      () => EditorState.createEmpty(),
-    );
-    const  [convertedContent, setConvertedContent] = useState(null);
-    const handleEditorChange = (state) => {
-      setEditorState(state);
-      convertContentToHTML();
-    }
-    const convertContentToHTML = () => {
-      let currentContentAsHTML = convertToHTML(editorState.getCurrentContent());
-      setConvertedContent(currentContentAsHTML);
-      setLesson_Body(currentContentAsHTML);
-    }
-    const createMarkup = (html) => {
-      return  {
-        __html: DOMPurify.sanitize(html)
-      }
-    }
-
-    const getFileBase64 = (file, callback) => {
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-      // Since FileReader is asynchronous,
-      // we need to pass data back.
-      reader.onload = () => callback(reader.result);
-      // TODO: catch an error
-      reader.onerror = error => {};
-    };
-    
-    const imageUploadCallback = file => new Promise(
-      (resolve, reject) => getFileBase64(
-        file,
-        data => resolve({ data: { link: data } })
-      )
-    );
+   
     
 
     return(
@@ -102,19 +134,13 @@ export default function NewLesson() {
                     onChange={e=>setLesson_Body(e.target.value)}
                 >
                 </textarea> */}
-                <Editor
-                  editorState={editorState}
-                  onEditorStateChange={handleEditorChange}
-                  wrapperClassName="wrapper-class"
-                  editorClassName="editor-class"
-                  toolbarClassName="toolbar-class"
-                  toolbar={{
-                    image: {
-                      uploadCallback: imageUploadCallback,
-                      previewImage: true,
-                    },
-                  }}
-                />
+                {/* <ReactQuill
+                  value={text}
+                  onChange={handleChange}
+                  modules={modules}
+                  formats={formats}
+                /> */}
+                <MyComponent />
             </div>
             <button className="newLessonButton" type="submit">
               Add Lesson
@@ -123,3 +149,4 @@ export default function NewLesson() {
         </div>
     );
 }
+
